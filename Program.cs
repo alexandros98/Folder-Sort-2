cc272a365868f2c7fa38241bcf330e358b631869
alexandros98 <alexproko123@gmail.com>
2022-01-26 16:03:24 +0200

    Strict file categorization

--- Program.cs
+++ Program.cs
@@ -15,12 +15,70 @@ namespace ConsoleApp1
             string dest = "C:\\Users\\Alexandros\\Desktop\\yeb2";
 
             //SUBDIRECTORIES DECLARATION
-            string subdirPDF = Path.Combine(dest, "PDF");
-            string subdirTXT = Path.Combine(dest, "TXT");
-            string subdirJPG = Path.Combine(dest, "JPG");
-            string subdirBRUH = Path.Combine(dest, "BRUH");
+            string subdir = Path.Combine(dest, "");
 
             string[] destFolders = Directory.GetDirectories(dest);
+            string[] fileEntries = Directory.GetFiles(folder);
+
+            if (fileEntries.Length == 0)
+            {
+                Console.WriteLine("No files to copy. The application will now exit.");
+                System.Environment.Exit(1);
+            }
+            if (destFolders.Length == 0)
+            {
+                foreach (string x in fileEntries)
+                {
+                    subdir = Path.Combine(dest, Path.GetExtension(x).TrimStart('.'));
+                    Directory.CreateDirectory(subdir);
+                    //Console.WriteLine(Path.GetExtension(x));
+
+                }
+                Console.WriteLine("Destination folder is empty. All subdirectories succesfully created based on the file extensions from the source folder.");
+                destFolders = Directory.GetDirectories(dest);
+            }
+            else
+            {
+                foreach (string x in fileEntries)
+                {
+                    foreach (string y in destFolders)
+                    {
+                        //Console.WriteLine("Comparing " + Path.GetExtension(x).TrimStart('.') + " and " + Path.GetFileName(y));
+                        if (Path.GetExtension(x).TrimStart('.') == Path.GetFileName(y))
+                        {
+                            Console.WriteLine("Directory " + Path.GetExtension(x).TrimStart('.') + " allready exists");
+                        }
+                        else if (Directory.Exists(Path.Combine(dest, Path.GetExtension(x).TrimStart('.'))) == false)
+                        {
+                            Directory.CreateDirectory(Path.Combine(dest, Path.GetExtension(x).TrimStart('.')));
+                            Console.WriteLine("Subdirectory created!");
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine("HELLO????");
+            foreach (string file in fileEntries)
+            {
+                fileName = Path.GetFileName(file);
+                fileExtension = Path.GetExtension(file);
+                foreach (string y in destFolders)
+                {
+                    //Console.WriteLine(file);
+                    //Console.WriteLine(Path.Combine(y, fileName));
+                    Console.WriteLine("Comparing " + fileExtension.TrimStart('.') + " and " + Path.GetFileName(y));
+                    if (fileExtension.TrimStart('.') == Path.GetFileName(y))
+                    {
+                        Console.WriteLine("File Moved!");
+                        File.Move(file, Path.Combine(y, fileName));
+                    }
+                }
+
+            };
+
+
+
+            /*
             
             if (destFolders.Length == 0)
             {
@@ -98,6 +156,7 @@ namespace ConsoleApp1
                     File.Move(file, Path.Combine(subdirBRUH, fileName));
                 }
             };
+            */
         }
     }
 }
